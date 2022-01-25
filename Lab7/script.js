ddbbd537cb3acde1d3d0b828dde346a95daf0e5f
Alonsomillan98 <you@example.com>
2022-01-25 12:14:07 -0600

    Avance

--- Lab7/script.js
+++ Lab7/script.js
@@ -2,6 +2,54 @@ $(document).ready(function() {
 
 // Start your code from here
 
+let animals = ["dog","cat","rabbit","hamster","frog"]
+
+
+for (let i = 0; i< animals.length; i++){
+    let a = $("<button>")
+    a.addClass("animal-button")
+    a.attr("data-type", animals[i])
+    a.text(animals[i])
+    $("#animal-buttons").append(a)
+}
+
+$("#animal-buttons").on("click", ".animal-button", function(){
+    
+    $("#animals").empty()
+
+    let search = $(this).attr("data-type")
+    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=TUvXdZitPWAt0qn2ZLjkBBIGwcUQn5yE"
+
+    $.ajax({
+        url: queryURL
+    })
+    .then(function(response){
+        
+        let results = response.data
+        
+        for (let i = 0; i < results.length; i++){
+
+            let animalDiv = $("<div class= \"animal-item\">")
+
+            let rating = results[i].rating
+
+            let p = $("<p>").text("Raiting: " + rating)
+
+            let animalImage = $("<img>")
+            animalImage.attr("src", results[i].images.fixed_height_still.url)
+            animalImage.attr("data-still",results[i].images.fixed_height_still.url)
+            animalImage.attr("data-animate",results[i].images.fixed_height.url )
+            animalImage.attr("data-state","still")
+
+            animalDiv.append(p)
+            animalDiv.append(animalImage)
+
+            $("#animals").append(animalDiv)
+        }
+    })
+
+})
+
 
 
 });
